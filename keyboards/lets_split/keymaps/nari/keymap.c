9df056df7174a0badf8278571609cf375aeb51ab
Narihiro Nakamura <authornari@gmail.com>
2018-03-06 19:43:18 +0900

    delete hold key

--- keyboards/lets_split/keymaps/nari/keymap.c
+++ keyboards/lets_split/keymaps/nari/keymap.c
@@ -6,12 +6,21 @@
 
 extern keymap_config_t keymap_config;
 
-#define BASE 0 // default layer
-#define SYMB 1 // symbol
-#define MDIA 2 // media keys
-#define LOWER 3
-#define RAISE 4
-#define FN 5   // reserved
+#define _BASE 0 // default layer
+#define _SYMB 1 // symbo
+#define _MDIA 2 // media keys
+#define _LOWER 3
+#define _RAISE 4
+#define _FN 5   // reserved
+
+enum planck_keycodes {
+  BASE = SAFE_RANGE,
+  SYMB,
+  MDIA,
+  LOWER,
+  RAISE,
+  FN
+};
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -23,18 +32,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------------+  +-----------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T |  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-----|  |------+------+------+------+------+------|
- * |Ctr/Ec|   A  |   S  |   D  |   F  |   G |  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * | Ctr  |   A  |   S  |   D  |   F  |   G |  |   H  |   J  |   K  |   L  |   ;  |Enter |
  * |------+------+------+------+------+-----|  |------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B |  |   N  |   M  |   ,  |   .  |   /  |  -   |
  * |------+------+------+------+------+-----|  |------+------+------+------+------+------|
  * | ESC  | GUI  |MDIA  | Alt  |Lower |Space|  |Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------  ------------------------------------------'
  */
-[BASE] = KEYMAP( \
-  KC_TAB,        KC_Q,           KC_W,               KC_E,          KC_R,                 KC_T,    KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  CTL_T(KC_ESC), KC_A,           KC_S,               KC_D,          KC_F,                 KC_G,    KC_H,    KC_J,        KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT,       KC_Z,           KC_X,               KC_C,          KC_V,                 KC_B,    KC_N,    KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
-  KC_ESC,        GUI_T(KC_LEFT), LT(MDIA, KC_DOWN), ALT_T(KC_UP),  LT(LOWER, KC_RGHT),   KC_SPC,  KC_SPC,  LT(RAISE,KC_LANG2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+[_BASE] = KEYMAP( \
+  KC_TAB,        KC_Q,           KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_LCTL,       KC_A,           KC_S,     KC_D,     KC_F,     KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_LSFT,       KC_Z,           KC_X,     KC_C,     KC_V,     KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
+  KC_ESC,        KC_LGUI,        MDIA,     KC_LALT,  LOWER,    KC_SPC,  KC_SPC,  RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Lower
@@ -48,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |  |      |      |      | PgUp |PgDwn |      |
  * `-----------------------------------------'  `-----------------------------------------'
  */
-[LOWER] = KEYMAP( \
+[_LOWER] = KEYMAP( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_DEL, \
   KC_DEL,  KC_TILD, KC_PIPE, KC_EQL,  KC_BSLS, KC_GRV,  KC_QUOT, KC_LCBR, KC_LBRC, KC_RBRC, S(KC_SCLN),KC_RCBR, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_DQT,  KC_PLUS, S(KC_COMM),S(KC_DOT),S(KC_SLSH),S(KC_MINS), \
@@ -66,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |  |      |      |      |      |      |      |
  * `-----------------------------------------'  `-----------------------------------------'
  */
-[RAISE] = KEYMAP( \
+[_RAISE] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_ENT, \
   _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______, \
@@ -84,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      | Rclk | Lclk |      |  |      |      |      | Mute |      |      |
  * `-----------------------------------------'  `-----------------------------------------'
  */
-[MDIA] =  KEYMAP( \
+[_MDIA] =  KEYMAP( \
   RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_DEL, \
   _______, _______, _______, KC_MS_U, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______, _______, \
@@ -92,4 +101,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 
 
+};
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case SYMB:
+      if (record->event.pressed) {
+        layer_on(_SYMB);
+      } else {
+        layer_off(_SYMB);
+      }
+      return false;
+      break;
+    case MDIA:
+      if (record->event.pressed) {
+        layer_on(_MDIA);
+      } else {
+        layer_off(_MDIA);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+      } else {
+        layer_off(_RAISE);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+      } else {
+        layer_off(_LOWER);
+      }
+      return false;
+      break;
+    case FN:
+      if (record->event.pressed) {
+        layer_on(_FN);
+      } else {
+        layer_off(_FN);
+      }
+      return false;
+      break;
+  }
+  return true;
 };
