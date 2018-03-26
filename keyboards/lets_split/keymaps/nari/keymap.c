593d92fb2820356b4044a8b0d2f670537be36902
Narihiro Nakamura <authornari@gmail.com>
2018-03-26 16:37:13 +0900

    add Home,End. invert Up,Down

--- keyboards/lets_split/keymaps/nari/keymap.c
+++ keyboards/lets_split/keymaps/nari/keymap.c
@@ -36,14 +36,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-----|  |------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B |  |   N  |   M  |   ,  |   .  |   /  |  -   |
  * |------+------+------+------+------+-----|  |------+------+------+------+------+------|
- * | ESC  | GUI  |MDIA  | Alt  |Lower |Space|  |Space |Raise | Left | Down |  Up  |Right |
+ * | ESC  | GUI  |MDIA  | Alt  |Lower |Space|  |Space |Raise | Left |  Up  | Down |Right |
  * `-----------------------------------------  ------------------------------------------'
  */
 [_BASE] = KEYMAP( \
   KC_TAB,        KC_Q,           KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL,       KC_A,           KC_S,     KC_D,     KC_F,     KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT,       KC_Z,           KC_X,     KC_C,     KC_V,     KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
-  KC_ESC,        KC_LGUI,        MDIA,     KC_LALT,  LOWER,    KC_SPC,  KC_SPC,  RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_ESC,        KC_LGUI,        MDIA,     KC_LALT,  LOWER,    KC_SPC,  KC_SPC,  RAISE,  KC_LEFT, KC_UP,   KC_DOWN,   KC_RGHT \
 ),
 
 /* Lower
@@ -54,14 +54,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |  |  "   |   +  |   <  |   >  |   ?  |  _   |
  * |------+------+------+------+------+------+  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |  |      |      |      | PgUp |PgDwn |      |
+ * |      |      |      |      |      |      |  |      |      | Home |  End | PgUp |PgDown|
  * `-----------------------------------------'  `-----------------------------------------'
  */
 [_LOWER] = KEYMAP( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_DEL, \
   KC_DEL,  KC_TILD, KC_PIPE, KC_EQL,  KC_BSLS, KC_GRV,  KC_QUOT, KC_LCBR, KC_LBRC, KC_RBRC, S(KC_SCLN),KC_RCBR, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_DQT,  KC_PLUS, S(KC_COMM),S(KC_DOT),S(KC_SLSH),S(KC_MINS), \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______\
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_PGUP, KC_PGDN\
 ),
 
 /* Raise
